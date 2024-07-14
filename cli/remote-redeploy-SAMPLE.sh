f87343e8b7ff6738bc10e6a4bc61bb2316114aa8
Vladimir Puchkovskiy <alcovp@gmail.com>
2024-07-14 14:23:03 +0200

    -add working deploy script

--- cli/remote-redeploy-SAMPLE.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Uploading files to server..."
-rsync -av -e ssh --exclude='*.iml' --exclude='package-lock.json' --exclude='node_modules' --exclude='.idea' --exclude='.env' --exclude='.git' --exclude='.gitignore' --exclude='cli' ./.. [USER]@[REMOTE_HOST]:~
-echo "Updading node packages..."
-ssh -t [USER]@[REMOTE_HOST] "cd currencies && npm install && pm2 reload currencies"
