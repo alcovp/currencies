f3f177d4e9eb2e7e8c9f6d31450e7c73bba25c54
Vladimir Puchkovskiy <alcovp@gmail.com>
2024-07-16 23:22:55 +0200

    -fix deploy script

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . ./../.env
 echo "Uploading files to" $SERVER_HOST "as" $SERVER_USER"..."
-rsync -av -e ssh --exclude='*.iml' --exclude='package-lock.json' --exclude='node_modules' --exclude='.idea' --exclude='.env' --exclude='.git' --exclude='.gitignore' --exclude='cli' ./.. $SERVER_USER@$SERVER_HOST:~
-echo "Updading node packages..."
+rsync -av -e ssh --exclude='*.iml' --exclude='package-lock.json' --exclude='node_modules' --exclude='.idea' --exclude='.env' --exclude='.git' --exclude='.gitignore' --exclude='cli' ./../ $SERVER_USER@$SERVER_HOST:~
+echo "Updating node packages..."
 ssh -t $SERVER_USER@$SERVER_HOST "cd currencies && npm install && pm2 reload currencies"
