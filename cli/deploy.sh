bd3ee6475ecf6ae203cd54e66e0f639d461532f6
Vladimir Puchkovskiy <alcovp@gmail.com>
2024-07-20 23:33:51 +0200

    -change deploy script to use yarn

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -3,4 +3,4 @@
 echo "Uploading files to" $SERVER_HOST "as" $SERVER_USER"..."
 rsync -av -e ssh --exclude='*.iml' --exclude='package-lock.json' --exclude='node_modules' --exclude='.idea' --exclude='.env' --exclude='.git' --exclude='.gitignore' --exclude='cli' ./../ $SERVER_USER@$SERVER_HOST:~
 echo "Updating node packages..."
-ssh -t $SERVER_USER@$SERVER_HOST "cd currencies && npm install && pm2 reload currencies"
+ssh -t $SERVER_USER@$SERVER_HOST "cd currencies && yarn && pm2 reload currencies"
